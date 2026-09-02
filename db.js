95d2a319f0007c04e9a1ff7b575573586baf35ec
owiredumantey8-sudo <owiredumantey8@gmail.com>
2026-09-02 09:13:19 +0000

    add ssl block to database connection

--- db.js
+++ db.js
@@ -9,7 +9,10 @@ const db = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.getConnection((err, connection) => {
